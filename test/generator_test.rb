25aff628a44e3b7196bb3abd1d526557feed7c7d
ParamagicDev <konnor5456@gmail.com>
2020-05-18 17:45:59 -0400

    working prototype right now

---
+++ test/generator_test.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+require 'minitest/autorun'
+
+# Taken from https://github.com/excid3/jumpstart/blob/master/test/template_test.rb
+class GeneratorTest < Minitest::Test
+  def setup
+    system('[ -d test_app ] && rm -rf test_app')
+  end
+
+  def teardown
+    setup
+  end
+
+  def test_generator_succeeds
+    output, err = capture_subprocess_io do
+      system('DISABLE_SPRING=1 rails new -m template.rb test_app')
+    end
+    assert_includes output, 'Successfully added Docker to your project!'
+  end
+end
